a846adbe7a3a94eb54470e1c7d6262a6651ed2b3
IhorRudyka <142403032+IhorRudyka@users.noreply.github.com>
2023-11-12 17:27:03 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,24 +6,38 @@ class Car {
         this.speed = speed;
     }
     increaseSpeed(speedI = 10) {
-        return `You have increased your speed on: ${speedI}kmh.  Current speed is:${this.speed + speedI}kmh` 
+        this.speed += speedI;
+        return this.speed;
     }
     decreaseSpeed(speedD = 10) {
-        return `You have dicreased your speed on: ${speedD}kmh.  Current speed is:${this.speed - speedD}kmh` 
+        this.speed -= speedD;
+        return this.speed;
     } 
 }
 class Truck extends Car {
+    constructor(model, mark, country, speed) {
+        super(model, mark, country, speed);
+    }
     takeWeight(weight){
         console.log(`Truck ${this.mark} ${this.model} can take ${weight}kg`)
     }
 }
 class Bus extends Car {
+    constructor(model, mark, country, speed) {
+        super(model, mark, country, speed);
+    }
     takePeople(people) {
         console.log(`Bus ${this.mark} ${this.model} can take ${people}`)
     }
 }
 class Transporter extends Car {
+    constructor(model, mark, country, speed) {
+        super(model, mark, country, speed);
+    }
     takeCars(cars) {
-        console.log(`Transporter ${this.mark} ${this.model} can take ${cars}kg`)
+        console.log(`Transporter ${this.mark} ${this.model} can carry ${cars} cars`)
     }
 }
+const truck = new Truck('BelAZ', '75710', 'Belarus', 25);
+const bus = new Bus('Daron', 'RM3500', 'USA', 60);
+const transporter = new Transporter('Rolfo', 'Arctic', 'Italy', 70);
